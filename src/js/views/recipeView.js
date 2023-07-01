4a8211c755b02ebd67a375afc0df1379da58567d
viniciustmgs <viniciustmgs@gmail.com>
2023-07-01 14:46:19 -0300

    small change

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from '../../../node_modules/fractional/index.js';
 import View from './view.js';
 
 class RecipeView extends View {
